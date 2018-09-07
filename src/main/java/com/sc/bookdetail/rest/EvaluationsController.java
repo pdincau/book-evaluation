b27f05c584c8a945816ccc4be31f25f0c95125ec
Paolo D'Incau <paolo.dincau@gmail.com>
2018-09-07 21:37:07 +0200

    Refactored

--- src/main/java/com/sc/bookdetail/rest/EvaluationsController.java
+++ src/main/java/com/sc/bookdetail/rest/EvaluationsController.java
@@ -13,10 +13,16 @@ public class EvaluationsController {
 
     @GetMapping("/evaluations/{isbn}")
     List<Evaluation> find(@PathVariable("isbn") String isbn) {
+
         System.out.println("Received request!");
+
         if (isbn.equals("12345")) {
-            return asList(new Evaluation("Great Book!"));
+            return asList(A_GREAT_BOOK_EVALUATION);
         }
-        return asList(new Evaluation("Amazing :)"), new Evaluation("Must Read!"));
+        return asList(AN_AMAZING_BOOK_EVALUATION, A_MUST_READ_EVALUATION);
     }
+
+    private static final Evaluation A_GREAT_BOOK_EVALUATION = new Evaluation("Great Book!");
+    private static final Evaluation AN_AMAZING_BOOK_EVALUATION = new Evaluation("Amazing :)");
+    private static final Evaluation A_MUST_READ_EVALUATION = new Evaluation("Must Read!");
 }
